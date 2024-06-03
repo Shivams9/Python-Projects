ee5c95f04c007c2036810129890f5a30625bb767
SHIVAM SINGH <91686685+Shivams9@users.noreply.github.com>
2024-06-04 04:16:23 +0530

    Update ATM.py

--- ATM.py
+++ ATM.py
@@ -11,7 +11,7 @@ while chances >= 0:
             print('Please Press 2 to Make a Withdrawl\n')
             print('Please Press 3 to pay in\n')
             print('Please Press 4 To Return Card\n')
-            option = int(input('What Would you like to choose?'))
+            option = int(input('What Would you like to choose? '))
             if option == 1:
                 print('Your Balance is ₹', balance,'\n')
                 restart = input('would You you like to go back? ')
@@ -20,10 +20,10 @@ while chances >= 0:
                     break
             elif option == 2:
                 option2 = ('y')
-                withdrawl = float(input('How Much Would you like to withdraw?\n@$10/@$20/@$40/@$60/@$80/@$100 : \n'))
+                withdrawl = float(input('How Much Would you like to withdraw?\n ₹10/₹20/₹40/₹06/80/₹100 : \n'))
                 if withdrawl in [10,20,40,60,80,100]:
                     balance = balance - withdrawl
-                    print('\nYour Balance is now @$',balance)
+                    print('\nYour Balance is now ₹',balance)
                     restart = input('Would You like to go back? ')
                     if restart in ('n','No','no','N'):
                         print('Thank You')
@@ -31,12 +31,12 @@ while chances >= 0:
                 elif withdrawl !=[10, 20, 40, 60, 80, 100]:
                     print('Invalid Amount, Please Re-Try\n')
                     restart =('y')
-                elif withdrawl ==1:
+                elif withdrawl == 1:
                     withdrawl = float(input('Please Enter Desired amount:'))
             elif option == 3:
                 Pay_in = float(input("How Much Would You Like To Pay In? "))
-                balance = balance +Pay_in
-                print('\nYour Balance is now @$', balance)
+                balance = balance + Pay_in
+                print('\nYour Balance is now ₹', balance)
                 restart = input('Would  You you like to go back? ')
                 if restart in ('n', 'NO', 'no', 'N'):
                     print('Thank - You')
@@ -50,8 +50,9 @@ while chances >= 0:
                 restart = ('y')
     elif pin !=('1234'):
         print('Incorrect Password')
-        chance = chances -1
+        chances = chances -1
         if chances == 0:
+            print('\nYou Lost 3-Chances!')
             print('\nNo more Tries')
             break
             
